e5819f74f8e1db05a187b3d0e3174e1a556fd4ee
Udinei Silva <ukaliko@gmail.com>
2019-10-21 21:53:23 -0400

    Reinstalando dependencias e package-lock e fazendo build de producao

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 ﻿export const API_CONFIG = {
-    baseUrl: "https://curso-spring-ionic-us.herokuapp.com",
+    //baseUrl: "https://curso-spring-ionic-us.herokuapp.com",
+    baseUrl: "http://localhost:8081",
     bucketBaseUrl: "https://curso-spring-ionic-us.s3-sa-east-1.amazonaws.com"
                     
                     
